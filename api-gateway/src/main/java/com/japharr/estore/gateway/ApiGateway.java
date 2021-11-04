346493e1722a86da35300e74646bf64294da96f7
Jelil Adesina <jtutoff@gmail.com>
2021-11-04 17:37:20 +0100

    added api-gateway and discovery-server

---
+++ api-gateway/src/main/java/com/japharr/estore/gateway/ApiGateway.java
@@ -0,0 +1,15 @@
+package com.japharr.estore.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ApiGateway {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGateway.class, args);
+	}
+
+}
