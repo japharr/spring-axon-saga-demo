ed399c973d0e5a3d1b67b055822434883a9e54a0
Jelil Adesina <jtutoff@gmail.com>
2021-11-04 18:17:40 +0100

    added query handler

---
+++ core-module/src/main/java/com/japharr/estore/core/query/FetchUserPaymentDetailQuery.java
@@ -0,0 +1,8 @@
+package com.japharr.estore.core.query;
+
+import lombok.Data;
+
+@Data
+public class FetchUserPaymentDetailQuery {
+    private String userId;
+}
---
+++ user-service/src/main/java/com/japharr/estore/user/core/query/handler/UserEventHandler.java
@@ -0,0 +1,28 @@
+package com.japharr.estore.user.core.query.handler;
+
+import com.japharr.estore.core.model.PaymentDetail;
+import com.japharr.estore.core.model.User;
+import com.japharr.estore.core.query.FetchUserPaymentDetailQuery;
+import org.axonframework.queryhandling.QueryHandler;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserEventHandler {
+    @QueryHandler
+    public User findUserPaymentDetails(FetchUserPaymentDetailQuery query) {
+        PaymentDetail paymentDetail = PaymentDetail.builder()
+                .cardNumber("123Card")
+                .cvv("123")
+                .name("SERGEY KARGOPOLOV")
+                .validUntilMonth(12)
+                .validUntilYear(2030)
+                .build();
+
+        return User.builder()
+                .firstName("Sergey")
+                .lastName("Kargopolov")
+                .userId(query.getUserId())
+                .paymentDetail(paymentDetail)
+                .build();
+    }
+}
